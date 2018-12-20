0a46a07da994397ac9ca0ef078f2ce6041da388f
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-19 20:25:47 -0600

    create initial svg element

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ req.onload = function() {
   json = JSON.parse(req.responseText);
   // d3.select("div#svg-container")
   //   .text(JSON.stringify(json));
-  // drawSvg();
+  drawSvg();
 };
 
 /** Create hidden tooltip div */
@@ -22,3 +22,25 @@ const tooltip = d3.select("body")
   //   d3.select(this).append("span").attr("id", "gdp");
   // })
 ;
+
+function drawSvg() {
+
+  /** Set initial svg dimensions to 16:10 ratio */
+  const w = 800;
+  const h = w / 1.6;
+
+  /** Set "margins" for the chart */
+  const margin = {top: h * .1, right: w * .07, bottom: h * .1, left: w * .06};
+
+  /** Set width and height for the chart */
+  const width = w - margin.left - margin.right;
+  const height = h - margin.top - margin.bottom;
+
+  /** Create svg element */
+  const svg = d3.select("main div#svg-container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  ;
+
+}
