836253723c47f576046f1bd87a1674458e6c614d
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-19 20:19:40 -0600

    created tooltip div

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceD
 req.send();
 req.onload = function() {
   json = JSON.parse(req.responseText);
-  d3.select("div#svg-container")
-    .text(JSON.stringify(json));
+  // d3.select("div#svg-container")
+  //   .text(JSON.stringify(json));
   // drawSvg();
 };
 
+/** Create hidden tooltip div */
+const tooltip = d3.select("body")
+  .append("div")
+  .attr("id", "tooltip")
+  .style("position", "absolute")
+  .style("z-index", "10")
+  .style("visibility", "hidden")
+  // .each( function() {
+  //   d3.select(this).append("span").attr("id", "date");
+  //   d3.select(this).append("span").attr("id", "gdp");
+  // })
+;
